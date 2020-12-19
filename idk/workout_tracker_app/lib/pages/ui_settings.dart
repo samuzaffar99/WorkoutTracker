234ef63adbcfdb8afac69e44e0d84869cd68adf9
Remolox <rahimmsyed@gmail.com>
2020-12-19 20:40:58 +0500

    edited profile and settings
    added pic to profile
    added toggle buttons to settings (might need some work)

--- idk/workout_tracker_app/lib/pages/ui_settings.dart
+++ idk/workout_tracker_app/lib/pages/ui_settings.dart
@@ -10,6 +10,8 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     int index = 4;
+    bool toggleMode = false;
+    bool toggleAlarm = false;
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color(0xFF141414),
@@ -32,152 +34,167 @@ class _SettingsState extends State<Settings> {
         ),
         body: ListView(
           children: [
-            SizedBox(height: 40,),
-        Opacity(
-        opacity: 0.5,
-        child: Expanded(
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),
+            SizedBox(
+              height: 40,
             ),
-            elevation: 5,
-            margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Container(
-                height: 400,
-                child: Column(
-                  children: [
-                    SizedBox(height: 20,),
-                    Row(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
+            Opacity(
+              opacity: 0.5,
+              child: Expanded(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  elevation: 5,
+                  margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Container(
+                        height: 400,
+                        child: Column(
                           children: [
-                            Text(
-                              "   Dark Mode",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 17,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w600
-                              ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      "   Dark Mode",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontSize: 17,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ],
+                                ),
+                                Switch(
+                                  inactiveThumbColor: Colors.white,
+                                  activeTrackColor: Colors.green[10],
+                                  activeColor: Colors.green,
+                                  value: toggleMode,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      toggleMode=value;
+                                    });
+                                  },
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            //Insert toggle button here
-                          ],
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 5,),
-                    Row(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text(
-                              "   Workout Alarm",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 17,
-                                color: Colors.black,
-                                  fontWeight: FontWeight.w600
-                              ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+
+                                  children: [
+                                    Text(
+                                      "   Workout Alarm",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontSize: 17,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ],
+                                ),
+                                Switch(
+                                  inactiveThumbColor: Colors.white,
+                                  activeTrackColor: Colors.green[10],
+                                  activeColor: Colors.green,
+                                  value: toggleAlarm,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      toggleAlarm=value;
+                                    });
+                                  },
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            //Insert toggle button here
-                          ],
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 150,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ButtonTheme(
-                          height: 45.0,
-                          minWidth: 150.0,
-                          child: OutlineButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                              Navigator.pushNamed(context, 'Sign In');
-                            },
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0),
+                            SizedBox(
+                              height: 110,
                             ),
-                            color: Colors.transparent,
-                            textColor: Colors.white,
-                            borderSide: BorderSide(
-                                color: Colors.black, width: 1.25),
-                            highlightedBorderColor: Colors.white,
-                            child: Row(
+                            Row(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                Text(
-                                  "Log Out",
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.black,
+                                ButtonTheme(
+                                  height: 45.0,
+                                  minWidth: 150.0,
+                                  child: OutlineButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                      Navigator.pushNamed(context, 'Sign In');
+                                    },
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30.0),
+                                    ),
+                                    color: Colors.transparent,
+                                    textColor: Colors.white,
+                                    borderSide: BorderSide(
+                                        color: Colors.black, width: 1.25),
+                                    highlightedBorderColor: Colors.white,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "Log Out",
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ],
                             ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 20,),
-                    Text(
-                      'Developed by:\nAhmed Khan\nRahim Muhammad\nAbdullah Muzaffar',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 5,),
-                    InkWell(
-                      onTap: () {
-                        //Insert new navigation page here
-                      },
-                      child: Center(
-                        child: RichText(
-                          text: TextSpan(
-                            text: 'View on PlayStore',
-                            style: TextStyle(color: Colors.blue),
-                          ),
-                        ),
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        //Insert new navigation page here
-                      },
-                      child: Center(
-                        child: RichText(
-                          text: TextSpan(
-                            text: 'Rate Us',
-                            style: TextStyle(color: Colors.blue),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                )
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              'Developed by:\nAhmed Khan\nRahim Muhammad\nAbdullah Muzaffar',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            InkWell(
+                              onTap: () {
+                                //Insert new navigation page here
+                              },
+                              child: Center(
+                                child: RichText(
+                                  text: TextSpan(
+                                    text: 'View on PlayStore',
+                                    style: TextStyle(color: Colors.blue),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            InkWell(
+                              onTap: () {
+                                //Insert new navigation page here
+                              },
+                              child: Center(
+                                child: RichText(
+                                  text: TextSpan(
+                                    text: 'Rate Us',
+                                    style: TextStyle(color: Colors.blue),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-      ),
           ],
         ),
       ),
