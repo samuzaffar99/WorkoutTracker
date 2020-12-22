337ff1872fec5b03606bcc6982c3c62882529667
Remolox <rahimmsyed@gmail.com>
2020-12-22 14:52:12 +0500

    push test

--- idk/workout_tracker_app/lib/pages/ui_workout.dart
+++ idk/workout_tracker_app/lib/pages/ui_workout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:workout_tracker_app/navigation.dart';
+import 'package:workout_tracker_app/user_data.dart';
 
 class Workout extends StatefulWidget {
   @override
@@ -7,9 +8,52 @@ class Workout extends StatefulWidget {
 }
 
 class _WorkoutState extends State<Workout> {
+  Widget trySignIn() {
+    return FutureBuilder(
+        future: getWorkout('5fe0732cb271d358089313e4'),
+        builder: (buildContext, AsyncSnapshot snapshot) {
+          if (snapshot.hasError)
+            throw snapshot.error;
+          else if (!snapshot.hasData) {
+            return Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else {
+            //print(snapshot);
+            print(snapshot.data);
+            print(snapshot.data);
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      snapshot.data["days"][0]["day"],
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          }
+          //  else if (snapshot.data["Username"] == controllerUN.text) {
+          //   return DietScreen();
+          // } else if (snapshot.data["Username"] == null) {
+          //   return Container(
+          //     child: Center(
+          //       child: Text("Wrong User"),
+          //     ),
+          //   );
+          // }
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    int index =2;
+    int index = 2;
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color(0xFF141414),
@@ -20,10 +64,7 @@ class _WorkoutState extends State<Workout> {
             Padding(
                 padding: EdgeInsets.only(right: 20.0),
                 child: GestureDetector(
-                  onTap: () {},
-                  child: Icon(Icons.dehaze_rounded)
-                )
-            ),
+                    onTap: () {}, child: Icon(Icons.dehaze_rounded))),
           ],
         ),
         bottomNavigationBar: Theme(
@@ -32,8 +73,31 @@ class _WorkoutState extends State<Workout> {
           ),
           child: NavigationBar(index),
         ),
-        body: ListView(
-          children: [],
+        body: Column(
+          children: [
+            Expanded(
+              child: trySignIn(),
+              // child: Opacity(
+              //   opacity: 0.5,
+              //   child: Card(
+              //     shape: RoundedRectangleBorder(
+              //       borderRadius: BorderRadius.circular(30),
+              //     ),
+              //     elevation: 5,
+              //     margin: EdgeInsets.fromLTRB(15.0, 15.0, 15.0, 400.0),
+              //     child: Padding(
+              //       padding: const EdgeInsets.all(15.0),
+              //       child: Row(
+              //         mainAxisAlignment: MainAxisAlignment.start,
+              //         children: [
+              //           Icon(Icons.info_outline_rounded),
+              //         ],
+              //       )
+              //     ),
+              //   ),
+              // ),
+            )
+          ],
         ),
       ),
     );
--- idk/workout_tracker_app/lib/user_data.dart
+++ idk/workout_tracker_app/lib/user_data.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'src/api.dart';
 import 'src/model.dart';
+import 'package:mongo_dart/mongo_dart.dart' as mongo;
 
-class UserDetails
-{
+class UserDetails {
   final Api _api = Api();
   Driver drivers;
   String username;
@@ -16,13 +16,54 @@ class UserDetails
 
   UserDetails({this.username});
 
-  setUserDetails() async
-  {
-    await _api.getDriver(username).then((value) {
-    gender=value.gender;
-    weight=value.weight;
-    height=value.height;
-    date=value.date;
-    });
+  setUserDetails() async {
+    await _api.getDriver(username).then(
+      (value) {
+        gender = value.gender;
+        weight = value.weight;
+        height = value.height;
+        date = value.date;
+      },
+    );
   }
+}
+
+Future<Map> getUserInfo(String user) async {
+  var db = await mongo.Db.create(
+      "mongodb+srv://Admin:admin@cluster0.ejodh.mongodb.net/WorkoutTracker");
+  var currentUser = db.collection('user');
+  await db.open();
+  var userDetails = await currentUser.findOne(mongo.where.eq('username', user));
+  await db.close();
+  if (userDetails == null) {
+    userDetails = {'username': null};
+  }
+  return userDetails;
+}
+
+Future<Map> getWorkout(String wid) async {
+  var db = await mongo.Db.create(
+      "mongodb+srv://Admin:admin@cluster0.ejodh.mongodb.net/WorkoutTracker");
+  var currentWorkout = db.collection('workout');
+  await db.open();
+  var workoutDetails = await currentWorkout.findOne(mongo.where.id(mongo.ObjectId.fromHexString(wid)));
+  await db.close();
+  if (workoutDetails == null) {
+    workoutDetails = {'_id': null};
+  }
+  print("called function");
+  return workoutDetails;
+}
+
+Future<Map> getExerciseInfo(String eid) async {
+  var db = await mongo.Db.create(
+      "mongodb+srv://Admin:admin@cluster0.ejodh.mongodb.net/WorkoutTracker");
+  var currentExercise = db.collection('exercise');
+  await db.open();
+  var exerciseDetails = await currentExercise.findOne(mongo.where.eq('_id', eid));
+  await db.close();
+  if (exerciseDetails == null) {
+    exerciseDetails = {'_id': null};
+  }
+  return exerciseDetails;
 }
